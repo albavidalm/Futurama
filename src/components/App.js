37da05b488cd75372ad9e559f1971c26a9d1dff2
Alba V <albavidalm@gmail.com>
2021-10-14 14:15:21 +0200

    Add basics

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "../stylesheets/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
