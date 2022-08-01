8c5024e4b5ce85f4c30400a1090f614848fa9968
albavidalm <albavidalm@gmail.com>
2022-08-01 19:56:52 +0200

    Add classes at card and fix body bg

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -14,60 +14,61 @@ const CharacterDetail = ({ characterDetail }) => {
     );
   };
   return (
-    <article className="detail">
+    <>
       <Link className="goback" to="/">
         <i className="fas fa-chevron-circle-left"></i>
         Go back
       </Link>
-
-      <div className="detail__card">
+      <article className="detail">
         <img
-          className="detail__card--img"
+          className="detail__img"
           src={characterDetail.picture}
           alt={`Imagen de ${characterDetail.name}`}
           title={characterDetail.name}
         />
 
-        <div className="detail__card--info">
-          <h2 className="detail__card--info-title">{characterDetail.name}</h2>
+        <div className="detail__info">
+          <h2 className="detail__info--title">{characterDetail.name}</h2>
           <section>
-            <ul className="detail__card--info-description">
-              <li>
-                <p className="boldtitle">Specie: </p>
+            <ul>
+              <li className="detail__info--description">
+                <p className="detail__info--description--text">Specie: </p>
                 {getIconSpecie(characterDetail.specie)}
                 {characterDetail.specie}
               </li>
-              <li>
-                <p className="boldtitle">Age: </p>
+              <li className="detail__info--description">
+                <p className="detail__info--description--text">Age: </p>
                 {characterDetail.age}
               </li>
-              <li>
-                <p className="boldtitle">Origin: </p>
+              <li className="detail__info--description">
+                <p className="detail__info--description--text">Origin: </p>
                 {characterDetail.origin}
               </li>
-              <li>
-                <p className="boldtitle">Occupation: </p>
+              <li className="detail__info--description">
+                <p className="detail__info--description--text">Occupation: </p>
                 {characterDetail.occupation}
               </li>
 
-              <li>
-                <p className="boldtitle">Said: </p>
-                {!saidValue ? (
-                  <p>{characterDetail.sayings[0]}</p>
-                ) : (
-                  <p>{saidValue}</p>
-                )}
+              <li className="detail__info--description said">
+                <p className="detail__info--description--text">Said: </p>
+                <div className="detail__info--description--random">
+                  {!saidValue ? (
+                    <p>{characterDetail.sayings[0]}</p>
+                  ) : (
+                    <p>{saidValue}</p>
+                  )}
+                  {characterDetail.sayings.length > 1 && (
+                    <button onClick={said} title="Press">
+                      Tell me more 🔀
+                    </button>
+                  )}
+                </div>
               </li>
             </ul>
-            {characterDetail.sayings.length > 1 && (
-              <button onClick={said} title="Press">
-                Tell me more 🔀
-              </button>
-            )}
           </section>
         </div>
-      </div>
-    </article>
+      </article>
+    </>
   );
 };
 
